29595d6bf37e6d1f997c0ba0483d15158b4ef8dd
hualian‑999 <1940492480@qq.com>
2026-09-15 05:08:14 +0800

    更新

--- AI大模型RAG与智能体开发/P3_LangChainRAG开发/02LangChain访问阿里云通义千问大模型.py
+++ AI大模型RAG与智能体开发/P3_LangChainRAG开发/02LangChain访问阿里云通义千问大模型.py
@@ -1,9 +1,18 @@
-# langchain_community
-from langchain_community.llms.tongyi import Tongyi
+# import os
 
-model=Tongyi(model="qwen-max")
+# langchain_community
+from langchain_community.chat_models.tongyi import ChatTongyi
+
+# api_key = os.getenv("DASHSCOPE_API_KEY")
+# if not api_key:
+#     raise ValueError(
+#         "请先设置环境变量 DASHSCOPE_API_KEY，示例：setx DASHSCOPE_API_KEY "
+#         '"你的API Key"'
+#     )
+
+model = ChatTongyi(model="qwen-max")
 
 # 调用invoke向模型提问
-res=model.invoke(input="你是谁,能做什么?")
+res = model.invoke(input="你是谁,能做什么?")
 
-print(res)
+print(res.content)
